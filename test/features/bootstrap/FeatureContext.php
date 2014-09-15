a8d491367617102d12f350be57f0ce88e3cd6a6e
Osman Ishmael <ishmael@mountbatten.net>
2014-09-15 16:46:38 +0300

    added context definitions

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -131,6 +131,67 @@ class FeatureContext extends DrupalContext
         $this->getSession()->getPage()->fillField('questionnaire_question_options[und][' . --$field_number . '][value]', $value);
     }
   
-   
+   /**
+    * @Given /^I am on the "([^"]*)"$/
+    */
+   public function iAmOnThe($arg1) {
 
-}
+   }
+   
+   /**
+ * @Given /^I am logged in as a user with the (\d+) role$/
+   */
+    public function iAmLoggedInAsAUserWithTheRole($arg1) {
+        
+    }
+    
+   /**
+    * @When /^i go to "([^"]*)"$/
+    */
+    
+    public function iGoTo($arg1) {
+    
+    }
+    
+    /**
+     * @Given /^I fill in question Options field (\d+) with "([^"]*)"$/
+     */
+    public function iFillInQuestionOptionsFieldWith($arg1, $arg2) {
+    
+    }
+    
+    /**
+     * @Given /^I am logged in as a user with (\d+) role$/
+     */
+    public function iAmLoggedInAsAUserWithRole($arg1) {
+        
+        
+    }
+    
+    /**
+     * @Given /^I click the Welcome Link$/
+     */
+    public function iClickTheWelcomeLink() {
+  
+    }
+    
+     /**
+      * @Given /^I fill in "([^"]*)" with (\d+)$/
+      */
+    public function iFillInWith($arg1, $arg2) {
+    
+    }
+    
+    /**
+     * @Given /^I go to the "([^"]*)"$/
+     */
+    public function iGoToThe($arg1) {
+   
+    }
+    
+    /**
+     * @Then /^I should see not "([^"]*)"$/
+     */
+    public function iShouldSeeNot($arg1) {
+
+    }
