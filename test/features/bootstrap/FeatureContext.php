646274b0643ee3cba9ca4b4afde1d2fdd06fa6b4
Osman Ishmael <ishmael@mountbatten.net>
2014-09-16 14:57:12 +0300

    Update FeatureContext.php

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -184,8 +184,9 @@ class FeatureContext extends DrupalContext
     
     /**
      * @Given /^I click the Welcome Link$/
-     *
+     */
     public function iClickTheWelcomeLink() {
+        $this->getSession()->getPage()->clickLink('Welcome');
   
     }
     
